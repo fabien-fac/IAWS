401623b265c2208994cb904555cc6cfadb8ae014
Joris <jorismichel@hotmail.fr>
2014-03-30 18:34:41 +0200

    ajout parser Station

--- client/TisseoVelib/src/org/iaws/classes/Station.java
+++ client/TisseoVelib/src/org/iaws/classes/Station.java
@@ -9,5 +9,60 @@ public class Station {
 	private int nbLike;
 	private int nbUnlike;
 	
+	public Station(String nom, String adresse, int totalVelo, int nbVeloDispo) {
+		super();
+		this.nom = nom;
+		this.adresse = adresse;
+		this.totalVelo = totalVelo;
+		this.nbVeloDispo = nbVeloDispo;
+	}
 
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public int getTotalVelo() {
+		return totalVelo;
+	}
+
+	public void setTotalVelo(int totalVelo) {
+		this.totalVelo = totalVelo;
+	}
+
+	public int getNbVeloDispo() {
+		return nbVeloDispo;
+	}
+
+	public void setNbVeloDispo(int nbVeloDispo) {
+		this.nbVeloDispo = nbVeloDispo;
+	}
+
+	public int getNbLike() {
+		return nbLike;
+	}
+
+	public void setNbLike(int nbLike) {
+		this.nbLike = nbLike;
+	}
+
+	public int getNbUnlike() {
+		return nbUnlike;
+	}
+
+	public void setNbUnlike(int nbUnlike) {
+		this.nbUnlike = nbUnlike;
+	}
+	
 }
