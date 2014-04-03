6cfd267aeab6779468c7ebd9c12b91a16588a5ba
Fabien <mon email>
2014-04-03 13:36:41 +0200

    plan zoomable et naviguable

--- client/TisseoVelib/src/org/iaws/fragment/HomeFragment.java
+++ client/TisseoVelib/src/org/iaws/fragment/HomeFragment.java
@@ -1,13 +1,18 @@
 package org.iaws.fragment;
  
 import org.iaws.R;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
  
 public class HomeFragment extends Fragment {
+	
+	private View rootView;
+	private WebView plan_view;
      
     public HomeFragment(){}
      
@@ -15,8 +20,20 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
   
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
           
+        init_composants();
+        
         return rootView;
+    }
+    
+    private void init_composants(){
+    	
+    	plan_view = (WebView) rootView.findViewById(R.id.home_webview_plan);
+    	plan_view.getSettings().setBuiltInZoomControls(true);
+    	plan_view.getSettings().setLoadWithOverviewMode(true);
+    	plan_view.getSettings().setUseWideViewPort(true);
+    	plan_view.loadUrl("file:///android_res/drawable/plan.png");
+    	
     }
 }
